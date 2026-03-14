a20dc17278a4c6539530c22daa0cfc34d8da87e7
Deepak <tehlandeepak31@gmail.com>
2026-03-14 18:37:32 +0530

    Refactor sigma function and add comments to make it readable
    Refactored sigma function and added comments for clarity.

--- source.py
+++ source.py
@@ -1,9 +1,10 @@
+# A Function that give Sum of given Integer
 def sigma(n):
     total = 0
     for i in range(1, n + 1):
         total = total + i
     return total
-
+# This will take value from User
 n = int(input("Enter a Number: "))
-
+# This will call the function
 print(sigma(n))
